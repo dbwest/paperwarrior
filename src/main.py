b6820fbb4db7e1a98cdaf6f8e35456a61baca7cf
David West <david.b.west@gmail.com>
2020-09-11 19:23:44 -0400

    foo

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from breadpi import BreadPi
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,9 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
+    bread_pi = BreadPi()
+    bread_pi.led_on('L1')
+    bread_pi.led_on('L2')
+    bread_pi.led_on('L3')
+    bread_pi.led_on('L4')
     app.run(host='0.0.0.0', port=80)
