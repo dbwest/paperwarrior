08d2d506074caae44c46c981ef7951978e878b5c
David West <david.b.west@gmail.com>
2020-09-11 21:30:51 -0400

    loop

--- src/main.py
+++ src/main.py
@@ -1,15 +1,28 @@
-from flask import Flask
 from breadpi import BreadPi
-app = Flask(__name__)
+import time
+# from flask import Flask
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+bread_pi = BreadPi()
 
-if __name__ == '__main__':
-    bread_pi = BreadPi()
+while True:
     bread_pi.led_on('L1')
     bread_pi.led_on('L2')
     bread_pi.led_on('L3')
     bread_pi.led_on('L4')
-    app.run(host='0.0.0.0', port=80)
+    time.sleep(1)
+    bread_pi.led_off('L1')
+    bread_pi.led_off('L2')
+    bread_pi.led_off('L3')
+    bread_pi.led_off('L4')
+    time.sleep(1)
+
+
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello_world():
+#     return 'Hello World!'
+#
+# if __name__ == '__main__':
+
+    # app.run(host='0.0.0.0', port=80)
